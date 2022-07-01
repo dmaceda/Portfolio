9ea0501ba71b5f4388b2d96fe16cc5e48eb9ff19
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-07-01 20:23:15 -0300

    change CV

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
         <li><a href='#about'>About</a></li>
         <li><a href='#experiences'>Experiences</a></li>
         <li><a href='#services'>Services</a></li>
-        <li><a href='portfolio'>Portfolio</a></li>
-        <li><a href='contact'>Contact</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
       </ul>
       <div className="footer__socials">
         <a href="https://www.facebook.com/diego.maceda.7/" target='_blank'> <AiFillFacebook/> </a>
