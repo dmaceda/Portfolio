98592837a097fd0a5c841c92bf0baa8025e6c822
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-07-11 10:33:54 -0300

    socials changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 import LOGO from '../../assets/logo_tipo.png'
-import {AiFillFacebook, AiOutlineInstagram, AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineGithub, AiFillLinkedin, AiOutlineTwitter} from 'react-icons/ai'
 const Footer = () => {
   return (
     <footer>
@@ -15,8 +15,8 @@ const Footer = () => {
         <li><a href='#contact'>Contact</a></li>
       </ul>
       <div className="footer__socials">
-        <a href="https://www.facebook.com/diego.maceda.7/" target='_blank'> <AiFillFacebook/> </a>
-        <a href="https://www.instagram.com/shaman.crew/" target='_blank'> <AiOutlineInstagram /> </a>
+        <a href="https://www.linkedin.com/in/dmaceda/" target='_blank'> <AiFillLinkedin/> </a>
+        <a href="https://github.com/dmaceda" target='_blank'> <AiOutlineGithub/> </a>
         <a href="https://twitter.com/?lang=es" target='_blank'> <AiOutlineTwitter/> </a>
       </div>
       <div className="footer__copyright">
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BsLinkedin, BsGithub, BsInstagram} from 'react-icons/bs'
+import {BsLinkedin, BsGithub, BsTwitter} from 'react-icons/bs'
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
         <a href='https://www.linkedin.com/in/dmaceda/' target='_blank' ><BsLinkedin/></a>
         <a href='https://github.com/dmaceda' target='_blank' ><BsGithub/></a>
-        <a href='https://www.instagram.com/shaman.crew' target='_blank' ><BsInstagram/></a>
+        <a href='https://www.twitter.com/dmaceda' target='_blank' ><BsTwitter/></a>
     </div>
   )
 }
