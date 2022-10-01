371402e6c71710431a858fec77ec38d30346fd60
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-10-01 17:51:37 -0300

    add proyects

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import ME from '../../assets/me2.png'
 import HeaderSocial from './HeaderSocial'
 const Header = () => {
   return (
+    <div id='#'>
     <header className='container header__container'>
       <h5>Hello! I'm</h5>
       <h1>Diego Maceda</h1>
@@ -14,6 +15,7 @@ const Header = () => {
       <a href="#contact" className="scroll_down">Scroll Down</a>
       <HeaderSocial />
     </header>
+    </div>
   )
 }
 
