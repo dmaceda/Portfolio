38d3b0c9e1946b95d3cbc9e689c3cb5974cce10f
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-10-01 18:21:36 -0300

    add proyects

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -8,7 +8,7 @@ const [activeNav,setActiveNav] = useState('#')
   return (
     <nav id='navbar'>
       <img className='logo_n' src={LOGO} alt='logo' width='40px'/>
-      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}>Home</a>
+      <a href='Portfolio/#' onClick={()=> setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}>Home</a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''}>About</a>
       <a href='#experiences' onClick={()=> setActiveNav('#experiences')} className={activeNav ==='#experiences' ? 'active' : ''}>Experience</a>
       <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} className={activeNav ==='#portfolio' ? 'active' : ''}>Proyects</a>
