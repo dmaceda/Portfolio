af417fc47533d3a3fa504e2e6724c4bbfd45c319
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-10-01 22:02:35 -0300

    add proyects

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
 const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='Portfolio/#' onClick={()=> setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='/#' onClick={()=> setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href='#experiences' onClick={()=> setActiveNav('#experiences')} className={activeNav ==='#experiences' ? 'active' : ''}><BiBook/></a>
       <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} className={activeNav ==='#portfolio' ? 'active' : ''}><RiBriefcase2Line/></a>
